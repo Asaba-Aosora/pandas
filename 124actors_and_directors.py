18d2f58ae40d2f00f3314d4c0b5b60e24b2c7bcb
Asaba_Aosora <2737169859@qq.com>
2026-01-18 20:10:09 +0800

    合作过至少三次的导演和演员

---
+++ 124actors_and_directors.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    df = actor_director.groupby(by=['actor_id', 'director_id'])['timestamp'].size().reset_index()
+    return df[df['timestamp']>=3][['actor_id', 'director_id']]
+
+if __name__ == '__main__':
+    data = [[1, 1, 0], [1, 1, 1], [1, 1, 2], [1, 2, 3], [1, 2, 4], [2, 1, 5], [2, 1, 6]]
+    actor_director = pd.DataFrame(data, columns=['actor_id', 'director_id', 'timestamp']).astype({'actor_id':'int64', 'director_id':'int64', 'timestamp':'int64'})
+    print(actors_and_directors(actor_director))
